bcfedf5a689327f0520b5410451f50367afd1c45
Hyunjun Jeon <hyunjunjeon@Hyunjunui-MacBook-Pro.local>
2016-09-12 19:32:50 +0900

    firebase realtime database and send trial

--- app/src/main/java/com/polllabears/www/pollla/MainActivity.java
+++ app/src/main/java/com/polllabears/www/pollla/MainActivity.java
@@ -2,25 +2,45 @@ package com.polllabears.www.pollla;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+
+    public static final String ANONYMOUS = "anonymous";
+    private static final String TAG = "MainActivity";
+    public static final String MESSAGES_CHILD = "messages";
+    private static final String MESSAGE_SENT_EVENT = "message_sent";
 
     private FirebaseAuth mFirebaseAuth;
     private FirebaseUser mFirebaseUser;
+    private GoogleApiClient mGoogleApiClient;
+    private DatabaseReference mFirebaseDatabaseReference;
+    private FirebaseAnalytics mFirebaseAnalytics;
 
-    public static final String ANONYMOUS = "anonymous";
-    private String mUsername;
-    private String mPhotoUrl;
+    private String mUsername; // 로그인한 사람 이름
+    private String mPhotoUrl; // 로그인한 프로필 사진 주소
+
+    private Button mSendButton;
+    private EditText mCardText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +51,11 @@ public class MainActivity extends AppCompatActivity {
 
         // Set default username is anonymous.
         mUsername = ANONYMOUS;
+
+
+        // Initialize Firebase Measurement.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
 
         // Initialize Firebase Auth
         mFirebaseAuth = FirebaseAuth.getInstance();
@@ -47,6 +72,30 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API)
+                .build();
+
+
+        // Initialize Firebase Realtime Database.
+        mFirebaseDatabaseReference = FirebaseDatabase.getInstance().getReference();
+
+        // 데이터베이스에 정보 보내서 저장하기
+        mCardText = (EditText) findViewById(R.id.cardText);
+        mSendButton = (Button) findViewById(R.id.sendButton);
+
+        mSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Card friendlyMessage = new Card(mCardText.getText().toString(), mUsername, mPhotoUrl);
+                mFirebaseDatabaseReference.child(MESSAGES_CHILD).push().setValue(friendlyMessage);
+                mCardText.setText("");
+                mFirebaseAnalytics.logEvent(MESSAGE_SENT_EVENT, null);
+            }
+        });
+
+        // 하단의 floating button 조작
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,17 +114,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public boolean onOptionsItemSelected(MenuItem item) { // 상단 탭 메뉴 버튼 조작
+        switch (item.getItemId()) {
+            case R.id.sign_out_menu:
+                mFirebaseAuth.signOut();
+                Auth.GoogleSignInApi.signOut(mGoogleApiClient);
+                mFirebaseUser = null;
+                mUsername = ANONYMOUS;
+                mPhotoUrl = null;
+                startActivity(new Intent(this, SignInActivity.class));
+                return true;
+            case R.id.action_settings:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d(TAG, "onConnectionFailed:" + connectionResult);
     }
 }
